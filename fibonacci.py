65dcf25b1893eeabb2bf5684ad0d767f1d30d3a5
Dylan Bargteil <dylan.bargteil@gmail.com>
2020-04-24 16:08:51 -0400

    script for computing fibonacci sequence

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+import sys
+
+def fibonacci(n):
+    low = 0
+    high = 1
+    sequence = [low, high]
+    for _ in range(2, n):
+        sequence.append(low + high)
+        temp = high
+        high = low + high
+        low = temp
+
+    return sequence[:n]
+
+if __name__ == '__main__':
+    print(fibonacci(int(sys.argv[1])))
