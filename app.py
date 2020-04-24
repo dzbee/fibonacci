5d98042d1cf3420ef8711b3cd98ec46d0a6ed609
Dylan Bargteil <dylan.bargteil@gmail.com>
2020-04-24 17:15:52 -0400

    add form for querying n fibonacci numbers

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import os
+
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 from services.fibonacci import register_fibonacci_endpoints
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY', 'testing')
 Bootstrap(app)
 
 register_fibonacci_endpoints(app)
--- services/fibonacci.py
+++ services/fibonacci.py
@@ -1,11 +1,24 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, SubmitField, validators
 
 from lib.fibonacci import fibonacci
 
+class FibonacciQuery(FlaskForm):
+    n = IntegerField("How many Fibonacci numbers do you want?",
+                     [validators.DataRequired()])
+    submit = SubmitField()
+
 def register_fibonacci_endpoints(app):
-    @app.route('/fibonacci')
+    @app.route('/fibonacci', methods=['GET', 'POST'])
     def fibonacci_query():
-        return render_template('fibonacci.html')
+        form = FibonacciQuery(request.form)
+
+        if request.method == 'POST' and form.validate():
+            n = form.n.data
+            return redirect(url_for('fibonacci_n', n=n))
+
+        return render_template('fibonacci.html', form=form)
 
     @app.route('/fibonacci/<n>')
     def fibonacci_n(n):
