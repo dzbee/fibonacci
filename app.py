7955fc244998b9a4c8c3bdcacb1344e89ed0a3be
Dylan Bargteil <dylan.bargteil@gmail.com>
2020-04-24 16:43:45 -0400

    enable flask-bootstrap in app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 from services.fibonacci import register_fibonacci_endpoints
 
 app = Flask(__name__)
+Bootstrap(app)
 
 register_fibonacci_endpoints(app)
 
